18aaf17ad88a3e4f8cf76408e09c92a190b060f2
Mccullahz <zylardmccullah@gmail.com>
2025-09-10 17:50:12 -0400

    moving android ui into internal

--- internal/ui/components/progress.go
+++
@@ -1,5 +0,0 @@
-// this is a progress bar implementation, mostly ripped from https://github.com/charmbracelet/bubbletea/tree/main/examples/progress-animated and adjusted for this use case
-
-package components
-
-
