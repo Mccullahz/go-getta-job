630c7b9ebe820d94ee1f643451499514f1d0a875
Mccullahz <zylardmccullah@gmail.com>
2025-11-29 22:04:45 -0500

    add auth messages to ui messages

--- internal/ui/messages/messages.go
+++ internal/ui/messages/messages.go
@@ -5,10 +5,16 @@ package messages
 import (
     "cliscraper/internal/backend/geo"
     "cliscraper/internal/utils"
+    "cliscraper/internal/ui/model"
 )
 
 type DoneMsg struct {
     Businesses []geo.Business
     Results    []utils.JobPageResult
     Err        error
+}
+
+type AuthDoneMsg struct {
+    User *model.UserInfo
+    Err  error
 }
